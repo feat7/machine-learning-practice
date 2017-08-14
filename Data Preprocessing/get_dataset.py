7900cb19590651dee9a3fd2ae1abd0eb9719b989
feat7 <vinaykhobragade99@gmail.com>
2017-08-14 18:45:59 +0530

    Data Preprocessing Template ready

--- Data Preprocessing/get_dataset.py
+++ Data Preprocessing/get_dataset.py
@@ -31,3 +31,10 @@ from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X = LabelEncoder()
 X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
 onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X.toarray()).toarray()
+labelencoder_Y = LabelEncoder()
+Y = labelencoder_Y.fit_transform(Y)
+
+# Split the dataset for training and testing
+from sklearn.cross_validation import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
